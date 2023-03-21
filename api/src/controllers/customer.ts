48e1810daa8ee96723e9b3b8bd74c66ae29b418a
LORDNR <ratchanons.work@gmail.com>
2023-03-21 17:39:44 +0700

    🎨 format

--- api/src/controllers/customer.ts
+++ api/src/controllers/customer.ts
@@ -4,29 +4,29 @@ import { prisma, reqValidation } from '../utils'
 import { SuccessResponse, ErrorResponse } from '../types/response'
 
 async function profile(req: Request, res: Response) {
-    const customerId = req.customerId
-    try {
-        const profile = await prisma.customer.findUniqueOrThrow({
-            where: {
-                id: customerId,
-            },
-            select: {
-                id: true,
-                email: true,
-                firstname: true,
-                lastname: true,
-                status: true,
-            },
-        })
-        return res.status(200).json({
-            status: 'success',
-            data: profile,
-        } as SuccessResponse)
-    } catch (error) {
-        return res
-            .status(400)
-            .json({ status: 'error', message: error } as ErrorResponse)
-    }
+	const customerId = req.customerId
+	try {
+		const profile = await prisma.customer.findUniqueOrThrow({
+			where: {
+				id: customerId,
+			},
+			select: {
+				id: true,
+				email: true,
+				firstname: true,
+				lastname: true,
+				status: true,
+			},
+		})
+		return res.status(200).json({
+			status: 'success',
+			data: profile,
+		} as SuccessResponse)
+	} catch (error) {
+		return res
+			.status(400)
+			.json({ status: 'error', message: error } as ErrorResponse)
+	}
 }
 
 export default { profile }
--- api/src/middlewares/auth.ts
+++ api/src/middlewares/auth.ts
@@ -5,34 +5,34 @@ import { ErrorResponse } from '../types/response'
 import config from '../config'
 
 declare global {
-    namespace Express {
-        interface Request {
-            customerId?: string
-        }
-    }
+	namespace Express {
+		interface Request {
+			customerId?: string
+		}
+	}
 }
 
 async function verifyToken(req: Request, res: Response, next: NextFunction) {
-    try {
-        if (!req.headers.authorization) {
-            throw 'Invalid authorization'
-        }
+	try {
+		if (!req.headers.authorization) {
+			throw 'Invalid authorization'
+		}
 
-        let token = req.headers.authorization
+		let token = req.headers.authorization
 
-        var decoded = jwt.verify(
-            token,
-            config.environments.SECRET,
-        ) as jwt.JwtPayload
+		var decoded = jwt.verify(
+			token,
+			config.environments.SECRET,
+		) as jwt.JwtPayload
 
-        req.customerId = decoded.id
+		req.customerId = decoded.id
 
-        return next()
-    } catch (error) {
-        return res
-            .status(401)
-            .json({ status: 'error', message: error } as ErrorResponse)
-    }
+		return next()
+	} catch (error) {
+		return res
+			.status(401)
+			.json({ status: 'error', message: error } as ErrorResponse)
+	}
 }
 
 export { verifyToken }
