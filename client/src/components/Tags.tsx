343cdaf303a2796f64ba634d39f192c350e69d42
LORDNR <ratchanons.work@gmail.com>
2023-03-21 14:36:33 +0700

    🎨 format

--- client/src/components/Tags.tsx
+++ client/src/components/Tags.tsx
@@ -4,30 +4,29 @@ import { Box, FormControl, Stack, TextField, Typography } from '@mui/material'
 import { useRef, useState } from 'react'
 
 export function Tags({ data, handleDelete }: any) {
-    return (
-        <Box
-            sx={{
-
-                height: '100%',
-                display: 'flex',
-                padding: '0.4rem',
-                margin: '0 0.5rem 0 0',
-                justifyContent: 'center',
-                alignContent: 'center',
-                backgroundColor: 'lightsalmon',
-                color: 'white',
-                borderRadius: '16px'
-            }}
-        >
-            <Stack direction="row" gap={1}>
-                <Typography>{data}</Typography>
-                <Cancel
-                    sx={{ cursor: 'pointer' }}
-                    onClick={() => {
-                        handleDelete(data)
-                    }}
-                />
-            </Stack>
-        </Box>
-    )
+	return (
+		<Box
+			sx={{
+				height: '100%',
+				display: 'flex',
+				padding: '0.4rem',
+				margin: '0 0.5rem 0 0',
+				justifyContent: 'center',
+				alignContent: 'center',
+				backgroundColor: 'lightsalmon',
+				color: 'white',
+				borderRadius: '16px',
+			}}
+		>
+			<Stack direction="row" gap={1}>
+				<Typography>{data}</Typography>
+				<Cancel
+					sx={{ cursor: 'pointer' }}
+					onClick={() => {
+						handleDelete(data)
+					}}
+				/>
+			</Stack>
+		</Box>
+	)
 }
